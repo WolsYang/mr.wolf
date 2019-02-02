87f064dd5230b9543834ddae00ed061eb56416e7
Wols <caniculawolf@hotmail.com>
2019-02-03 01:20:42 +0800

    try game  function

--- app/controllers/chatbot_controller.rb
+++ app/controllers/chatbot_controller.rb
@@ -20,20 +20,22 @@ class ChatbotController < ApplicationController
 	#	傳送訊息給LINE reply_to_line(reply_text)
 		params['events'].each do |event|
 			text = received_text(event)
-			#查天氣
-				reply_image = get_weather(text)
-				#有查到後面的就不做了
+				#查天氣
+				"""
+				#reply_image = get_weather(text)
+				#觸發到到後面的事件就不跑了
 				unless reply_image.nil?
-					#傳送訊息到LINE
+				#	#傳送訊息到LINE
 					response = reply_image_to_line(reply_image)
 					#回應200
 					head :ok
 					return
 				end
-
+				"""
 				#記錄頻道
 				Channel.find_or_create_by(channel_id: channel_id)
 
+				"""
 				#學說話
 				reply_text = learn(channel_id, text)
 
@@ -46,19 +48,24 @@ class ChatbotController < ApplicationController
 				# 記錄對話
 				save_to_received(channel_id, text)
 				save_to_reply(channel_id, reply_text)
+				
 
 				# 傳送訊息給LINE
 				response = reply_to_line(reply_text)
+				"""
+
 
 				# 回應200
 				head :ok
 		end
 	end
+	"""
 	#查天氣方法
 	def get_weather(received_text)
 		return nil unless received_text.include? '天氣'
 		upload_to_imgur(get_weather_from_cwb)
 	end
+
 	#取得最新雷達回波圖
 	def get_weather_from_cwb
 		uri = URI('https://www.cwb.gov.tw/V7/js/HDRadar_1000_n_val.js')
@@ -67,6 +74,7 @@ class ChatbotController < ApplicationController
 		end_index = response.index('"),') - 1
 		"https://www.cwb.gov.tw" + response[start_index..end_index]
 	end
+
 	#上傳圖片到imgur
 	def upload_to_imgur(image_url)
 		#準備發送 Post request
@@ -92,6 +100,7 @@ class ChatbotController < ApplicationController
 			nil
 		end
 	end
+
 	#傳送圖片到Lline
 	def reply_image_to_line(reply_image)
 		return nil if reply_image.nil?
@@ -109,6 +118,7 @@ class ChatbotController < ApplicationController
 		#t傳送訊息
 		line.reply_message(reply_token, message)
 	end
+	'''
 	# 頻道ID
 	def channel_id
 		source = params['events'][0]['source']
@@ -118,17 +128,20 @@ class ChatbotController < ApplicationController
 		#return source['roomID'] unless source['roomID'].nil?
 		#source['userID']
 	end
+	'''
 	# 儲存對話
 	def save_to_received(channel_id, received_text)
 		return if received_text.nil?
 		Received.create(channel_id: channel_id, text: received_text)
-	end 
+	end
+
 	#儲存回應
 	def save_to_reply(channel_id, reply_text)
 		return if reply_text.nil?
 		Reply.create(channel_id: channel_id, text: reply_text)
 	end
-	#ech人云亦云
+
+	#echo人云亦云
 	def echo2(channel_id, received_text)
 		#如果在chanel_id最近沒人講過received_text,就不回應
 		recent_received_texts = Received.where(channel_id: channel_id).last(5)&.pluck(:text)
@@ -140,6 +153,7 @@ class ChatbotController < ApplicationController
 		
 		received_text
 	end
+
 	# 取得對方說的話
 	def received_text(event)
 		message = event['message']
@@ -150,6 +164,7 @@ class ChatbotController < ApplicationController
 		#	message['text']
 		#end
 	end
+
 	#學說話 同時記錄不同CHANEL的教學內容
 	def learn(channel_id, received_text)
 		#如果開頭不是學說話關鍵字; 就跳出
@@ -168,6 +183,7 @@ class ChatbotController < ApplicationController
 		KeywordMapping.create(channel_id: channel_id, keyword: keyword,message: message)
 		'是!!小得遵命'
 	end
+
 	#關鍵字回覆
 	def keyword_reply(channel_id, received_text)
 		message = KeywordMapping.where(channel_id: channel_id, keyword: received_text).last&.message
@@ -183,6 +199,51 @@ class ChatbotController < ApplicationController
 		#	mapping.message
 		#end
 	end
+	'''
+	def game(channel_id, received_text)
+		return nil unless received_text[0...6] == '我要玩遊戲'
+		
+		# 取得reply token
+		reply_token = params['events'][0]['replyToken']
+				
+		# 設定回覆訊息
+		message = {
+  			"type": "template",
+		 	"altText": "This is a buttons template",
+			"template": {
+			    "type": "buttons",
+			    "title": "Menu",
+			    "text": "請選擇您要玩的遊戲",
+      			"actions": [
+          			{
+            		"type": "postback",
+           			"label": "終極密碼",
+            		"data": "action=buy&itemid=123"
+            		"displayText" : "displayText"
+          			},
+          			{
+			        "type": "postback",
+			        "label": "射龍門",
+			        "data": "action=add&itemid=123"
+			        },
+			        {
+			        "type": "吹牛",
+			        "label": "View detail",
+			        "text": "http://example.com/page/123"
+			        }
+			        {
+			        "type": "殺手",
+			        "label": "View detail",
+			        "uri": "http://example.com/page/123"
+			        }
+      ]
+  }
+}
+		
+		# 傳送訊息 一個方法的回傳值是最後一行的結果
+		line.reply_message(reply_token, message)
+	end
+
 	#傳送訊息到LINE
 	def reply_to_line(reply_text)
 		return nil if reply_text.nil?
@@ -207,4 +268,6 @@ class ChatbotController < ApplicationController
 			config.channel_token ='VsnoSZR++5ejxl+LTwHVL8bHnEVi9xDozwQ5ajtK9t+BtGEn/Jt54fDBMFq0dO93rFJp7bwnz4ta0k/3DVqpReRlTFJSoEl+IG8S4CO+ucvA0j/rZ8Lsc/tjWRzLWCdFgR3BKOJNn8HdxOXZpw19mAdB04t89/1O/w1cDnyilFU='
 		}
 	end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,6 @@
-Rails.application.routes.draw do
-  devise_for :users
-  resources :keyword_mappings
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :push_messages, only: [:new, :create]
-  
-  get '/eat', to: 'chatbot#eat'
-  get '/request_headers', to: 'chatbot#request_headers'
-  get '/request_body', to: 'chatbot#request_body'
-  get '/response_headers', to: 'chatbot#response_headers'
-  get '/response_body', to: 'chatbot#show_response_body'
-  get '/sent_request', to: 'chatbot#sent_request'
-  
+Rails.application.routes.draw do |
+|  
   post '/webhook', to: 'chatbot#webhook'
+  
 end
 
