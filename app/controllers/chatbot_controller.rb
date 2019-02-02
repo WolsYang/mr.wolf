c5dfc13883b926d1405d967146f8756e85beaf92
Wols <caniculawolf@hotmail.com>
2019-02-03 02:09:40 +0800

    Update chatbot_controller.rb

--- app/controllers/chatbot_controller.rb
+++ app/controllers/chatbot_controller.rb
@@ -55,8 +55,7 @@ class ChatbotController < ApplicationController
           			{
             		"type": "postback",
            			"label": "終極密碼",
-            		"data": "action=buy&itemid=123",
-            		"displayText" : "displayText"
+            		"data": "action=buy&itemid=123"
           			},
           			{
 			        "type": "postback",
