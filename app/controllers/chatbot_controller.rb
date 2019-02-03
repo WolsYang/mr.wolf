129fdcbb27b81510f4401a90891030a04b3e8089
Wols <caniculawolf@hotmail.com>
2019-02-03 14:19:24 +0800

    Update chatbot_controller.rb

--- app/controllers/chatbot_controller.rb
+++ app/controllers/chatbot_controller.rb
@@ -54,7 +54,8 @@ class ChatbotController < ApplicationController
 	end
 
 	def game(channel_id, received_text)
-		return nil unless received_text[0...6] == '我要玩遊戲'			
+		return nil unless received_text[0...6] == '我要玩遊戲'
+		"玩遊戲囉"			
 	end
 
 	#傳送訊息到LINE
