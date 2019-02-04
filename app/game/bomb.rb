0ce8537bbe72b6a204b91302ee62079b3c1174eb
Wols <caniculawolf@hotmail.com>
2019-02-05 00:24:41 +0800

    .

--- app/game/bomb.rb
+++ app/game/bomb.rb
@@ -1,34 +1,37 @@
 class Bomb
-  def initialize(channel_id)
-    @now_max = 10000
-    @now_min = 1
-      @code = rand(1..@now_max-1)
-      @user_number = 0
-      @channel_id = channel_id
+    def initialize(channel_id)
+        @now_max = 10000
+      @now_min = 1
+        @code = rand(1..@now_max-1)
+        @user_number = 0
+        @channel_id = channel_id
     end
 
-  def c
+    def channal_id
       @channel_id 
     end
 
-    def play(user_number = @user_number, now_max = @now_max, now_min = @now_min)
+    def code
+      @code
+    end
+
+    def play(user_number = @user_number, now_max = @now_max, now_min = @now_min, code = @code)
       bomb = Bomb.find_by(channel_id: @channel_id)
-      channel = Channel.find_by(channel_id: channel_id)
+      channel = Channel.find_by(channel_id: @channel_id)
       if now_min < user_number && user_number < now_max
-          if user_number == @code
+          if user_number == code
             bomb.destroy
             channel.update(now_gaming: "no")
-            puts "恭喜你!!爆爆爆了"
-          elsif user_number > @code 
-            channel.update (now_max: user_number)
-            puts "#{now_min.to_s}~#{now_max.to_s}"
-          elsif user_number < @code
-            channel.update (now_min: user_number)
-            puts "#{now_min.to_s}~#{now_max.to_s}"
+            "恭喜你!!爆爆爆了"
+          elsif user_number > code 
+            channel.update(now_max: user_number)
+            now_min.to_s + " ~ " + now_max.to_s
+          elsif user_number < code
+            channel.update(now_min: user_number)
+            now_min.to_s + " ~ " + now_max.to_s
           end
       else
-          puts "您猜的數字不在範圍內 "
-          puts "請輸入範圍內的數字   " + now_min.to_s + " ~ " + now_max.to_s
+          "您猜的數字不在範圍內\n" '輸入範圍內的數字     ' + now_min.to_s + " ~ " + now_max.to_s
       end 
     end
 
--- db/migrate/20190204133956_remove_game_colum_and_add_game_status.rb
+++ db/migrate/20190204133956_remove_game_colum_and_add_game_status.rb
@@ -1,6 +1,6 @@
 class RemoveGameColumAndAddGameStatus < ActiveRecord::Migration[5.2]
   def change
-  	  remove_column :channels, :bomb
+  	remove_column :channels, :bomb
   	remove_column :channels, :dice
   	remove_column :channels, :porker
   	remove_column :channels, :wolf
