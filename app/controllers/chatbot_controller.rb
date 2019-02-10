2a24f7d7ef486f19a7e3cb0abb506355b6e0b99c
Wols <caniculawolf@hotmail.com>
2019-02-11 01:50:18 +0800

    Update chatbot_controller.rb

--- app/controllers/chatbot_controller.rb
+++ app/controllers/chatbot_controller.rb
@@ -20,7 +20,7 @@ class ChatbotController < ApplicationController
 	#	傳送訊息給LINE reply_to_line(reply_text)
 		params['events'].each do |event|
 			text = received_text(event)
-			puts userID
+			userID = params['events'][0]['source']['userId']
 				#記錄頻道
 				channel = Channel.find_or_create_by(channel_id: channel_id)
 				reply_text = game_keyword_reply(channel_id, text)
