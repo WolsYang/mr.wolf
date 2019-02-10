987bd7918223e904b9d3b2a5488d2d03bcd1408c
Wols <caniculawolf@hotmail.com>
2019-02-11 01:54:23 +0800

    Update chatbot_controller.rb

--- app/controllers/chatbot_controller.rb
+++ app/controllers/chatbot_controller.rb
@@ -25,7 +25,7 @@ class ChatbotController < ApplicationController
 				channel = Channel.find_or_create_by(channel_id: channel_id)
 				reply_text = game_keyword_reply(channel_id, text)
 				response = get_uer_profile(userID)#reply_to_line(reply_text)
-				profiile = line.get_profile(user_id)
+				profiile = line.get_profile(user_ID)
 				puts profiile.display_name
 				# 回應200
 				head :ok
