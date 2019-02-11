3600b703aad0ca9e81c045cc98b7013b60746f9d
Wols <caniculawolf@hotmail.com>
2019-02-11 21:33:40 +0800

    Update chatbot_controller.rb

--- app/controllers/chatbot_controller.rb
+++ app/controllers/chatbot_controller.rb
@@ -17,18 +17,6 @@ class ChatbotController < ApplicationController
 	#	推擠 (ECHO) echo2(channel_id, received_text) if  reply_text.nil?
 	#	記錄對話 save_to_received(channel_id, received_text), save_to_reply(channel_id, reply_text)
 	#	傳送訊息給LINE reply_to_line(reply_text)
-		if params['events']['message']['text'] == "+1"
-			profile = line.get_profile(params['events'][0]['source']['userId'])
-			case profile
-				when Net::HTTPSuccess then
-					contact = JSON.parse(response.body)
-					p contact['displayName']
-					p contact['pictureUrl']
-					p contact['statusMessage']
-			else
-				p "#{response.code} #{response.body}"
-			end
-		else
 			channel = Channel.find_or_create_by(channel_id: channel_id)
 			params['events'].each do |event|
 			text = received_text(event)
@@ -36,7 +24,6 @@ class ChatbotController < ApplicationController
 				reply_text = game_keyword_reply(channel_id, text)
 				response = #reply_to_line(reply_text)
 			end
-		end
 		# 回應200
 		head :ok
 	end
