decc2c78ce677dd1093c0848e10e4af990473d3c
Wols <caniculawolf@hotmail.com>
2019-02-12 16:06:47 +0800

    Update chatbot_controller.rb

--- app/controllers/chatbot_controller.rb
+++ app/controllers/chatbot_controller.rb
@@ -23,13 +23,14 @@ class ChatbotController < ApplicationController
 		puts params['events'][0]['source']['userId']
 			#記錄頻道				
 			reply_text = game_keyword_reply(channel_id, text)
-			unless params['events'][0]['message']['text'] == "+1"
+			if params['events'][0]['message']['text'] == "+1"
 				puts "成功功功功功功功功功功功功功"
-				puts reply_text
-				response = reply_to_line(reply_text) 
+				response = get_user_name(text)
 			else
 				puts "失敗敗敗敗敗敗敗敗敗敗敗敗敗敗敗敗敗敗"
-				response = get_user_name(text)
+				
+				puts reply_text
+				response = reply_to_line(reply_text) 
 			end
 			case response
 			when Net::HTTPSuccess then
@@ -83,12 +84,12 @@ class ChatbotController < ApplicationController
 		if received_text[0...5] == '我要玩遊戲'	&& channel.now_gaming == "no"
 			puts "成功了成功了成功了成功了成功了成功了成功了成功了成功了成功了成功了"
 			"玩遊戲囉"
-		#elsif received_text[0...6] == '我要玩遊戲'
+		#elsif received_text[0...5] == '我要玩遊戲'
 			#	"您還有遊戲進行中"
 		elsif channel.now_gaming == "Bomb" && received_text.match(%r{\D}).nil? == true
 			user_number = Bomb.guess(received_text)
 			Bomb.play(user_number, channel_id)
-		elsif channel.now_gaming == "Shoot" 
+		elsif channel.now_gaming == "shoot" 
 			ShootTheGate.shoot(received_text: received_text, channel_id: channel_id)
 		elsif received_text[0...4] == 'WY遊戲'
 			case received_text[4...8]
@@ -98,7 +99,7 @@ class ChatbotController < ApplicationController
 					Bomb.start(channel_id)
 					"開始拉~~範圍是 1 ~ 10000\n請輸入心中所想的整數\n例如:4841\n若是猜到密碼炸彈就引爆啦\n來看看誰這麼Lucky阿~"
 				when "shoo"
-					channel.update(now_gaming: received_text[4...8])
+					channel.update(now_gaming: received_text[4...9)
 					ShootTheGate.shoot(received_text: received_text, channel_id: channel_id)
 					"開始拉~~輸入\"抽\"抽取門柱\n輸入\"射\"抽取射門牌\n若射門牌數字介於門柱牌數字中間就贏啦~\n輸入\"重抽\"換一副牌重新開始"			
 			end
@@ -130,7 +131,7 @@ class ChatbotController < ApplicationController
           				{
             			"type": "postback",
            				"label": "射龍門",
-            			"data": "WY遊戲shoo3345678",
+            			"data": "WY遊戲shoot3345678",
             			"dataText": "玩玩玩玩玩玩玩"
           				}
       				]
