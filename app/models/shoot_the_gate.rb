5bce4d8173a4addb113faf2a14e72387d4019cd6
Wols <caniculawolf@hotmail.com>
2019-02-13 15:11:54 +0800

    Update shoot_the_gate.rb

--- app/models/shoot_the_gate.rb
+++ app/models/shoot_the_gate.rb
@@ -27,7 +27,7 @@ class ShootTheGate < ApplicationRecord
         end
       end
       bet = game.stakes if bet.to_i > game.stakes #獎金池&最大注 
-      self.shoot("射", channel_id, bet.to_i)
+      ShootTheGate.shoot("射", channel_id, bet.to_i)
     end
      
     case received_text
