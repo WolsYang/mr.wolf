9a0b5f6e8ec22424c445ef0dbeffef94ee2646f7
Wols <caniculawolf@hotmail.com>
2019-02-18 16:28:59 +0800

    Update sidekiq.rb

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://redis.example.com:7372/0' }
+  config.redis = { url: 'redis://redistogo:4d791ccf54436ffa23cb1aa261630324@dory.redistogo.com:10102/' }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://redis.example.com:7372/0' }
+  config.redis = { url: 'redis://redistogo:4d791ccf54436ffa23cb1aa261630324@dory.redistogo.com:10102/' }
 end
