32d878aa74a8ad1ee811b8235156ba48aac642ba
Wols <caniculawolf@hotmail.com>
2019-02-20 14:43:06 +0800

    Update chatbot_controller.rb

--- app/controllers/chatbot_controller.rb
+++ app/controllers/chatbot_controller.rb
@@ -70,7 +70,7 @@ class ChatbotController < ApplicationController
 				user_name = get_user_name(user_id)
 				player = Killer.to_gameid(user_id, user_name)
 				p kill.game_begin.to_s
-			if kill.game_begin == true & received_text == "+1"
+			if kill.game_begin.to_s == "true" & received_text == "+1"
 				p "+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1"
 				#判斷player是否已存在
 				REDIS.rpush(channel_id, player)
