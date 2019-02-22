91f3832a6a45f45feae765b467a732ed13e8dcbd
Wols <caniculawolf@hotmail.com>
2019-02-23 01:04:26 +0800

    Update chatbot_controller.rb

--- app/controllers/chatbot_controller.rb
+++ app/controllers/chatbot_controller.rb
@@ -17,7 +17,7 @@ class ChatbotController < ApplicationController
 		params['events'].each do |event|
 		text = received_text(event)
 			#記錄頻道	
-			ChatbotController.new.push_to_line(channel_id, Killer.reply_message("你是殺手,你唯一且必須的任務就是殺光所有生還者"))			
+			ChatbotController.new.push_to_line(channel_id, Killer.reply_message("你是殺手,你唯一且必須的任務就是殺光所有生還者"), "bomb")			
 			reply_text = game_keyword_reply(channel_id, text)
 			#if text == "+1"#統計+1數
 				#response = get_user_name(params['events'][0]['source']['userId'])
