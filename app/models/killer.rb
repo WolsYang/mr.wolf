365380ac7904abc8b9d1d97e0812815fe3d6d606
Wols <caniculawolf@hotmail.com>
2019-02-27 02:26:52 +0800

    Update killer.rb

--- app/models/killer.rb
+++ app/models/killer.rb
@@ -167,10 +167,10 @@ class Killer < ApplicationRecord
                 died_player = vote_result[n][44...-1].to_s + " "
             end
             text = "玩家" + died_player +"已被表決處死 \n但他不是兇手...真正的凶手可以選擇要不要殺他滅口"
-            replay_text = Killer.reply_message("text", "confirm", "vote_result")
+            reply_text = Killer.reply_message("text", "confirm", "vote_result")
             p reply_text
             p "SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS"
-            ChatbotController.new.push_to_line(channel_id, replay_text, "kill")
+            ChatbotController.new.push_to_line(channel_id, reply_text, "kill")
         end
     end
 
