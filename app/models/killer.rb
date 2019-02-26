4ac7b64ed23590746c97ed3f73f2b8f507cabf87
Wols <caniculawolf@hotmail.com>
2019-02-26 16:46:30 +0800

    Update killer.rb

--- app/models/killer.rb
+++ app/models/killer.rb
@@ -71,7 +71,8 @@ class Killer < ApplicationRecord
                 REDIS.set("jid"+channel_id, jid)
             end
             result = Killer.check_vote(player, channel_id, voted_player)
-            p result
+            p REDIS.get(player).to_i
+            P "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
             replytext = Killer.vote(channel_id) if REDIS.get(channel_id) == kill.players.size #都投完票才開始計算結果 減輕資料庫負擔
             # menu + reply_text
         elsif kill.players.size <= 2
