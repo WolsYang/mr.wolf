45db039b94306c3b6dcdcdb1cc867f87d1e33edf
Wols <caniculawolf@hotmail.com>
2019-02-26 14:22:11 +0800

    Update shoot_the_gate.rb

--- app/models/shoot_the_gate.rb
+++ app/models/shoot_the_gate.rb
@@ -43,20 +43,21 @@ class ShootTheGate < ApplicationRecord
           bet = received_text[2..n]
         end
       end
+      bet = game.stakes if bet.to_i > game.stakes #獎金池&最大注 
+      bet = bet.to_i
+      received_text = "射"
     elsif received_text =~ /^抽\d*/ && game.gambling == "Yes"
-        bet = basic_bet if received_text[1].nil?
-        return "獎金池沒了...請重新輸入\"小賭怡情\"設定" if game.stakes == 0
-        (1...received_text.size).each do |n|
-          unless received_text[n].match(%r{[0-9]|\s}).nil?
-            bet = received_text[1..n]
-          end
+      bet = basic_bet if received_text[1].nil?
+      return "獎金池沒了...請重新輸入\"小賭怡情\"設定" if game.stakes == 0
+      (1...received_text.size).each do |n|
+        unless received_text[n].match(%r{[0-9]|\s}).nil?
+          bet = received_text[1..n]
         end
-    end
       bet = game.stakes if bet.to_i > game.stakes #獎金池&最大注 
       bet = bet.to_i
       received_text = "射"
     end
-     
+
     case received_text
       when  "重抽"
         poker = Poker.shuffle(1)
