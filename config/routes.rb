9077f6681ca2030791ff516016782a3737f47d0c
Wols <caniculawolf@hotmail.com>
2019-03-10 20:30:42 +0800

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do 
   post '/webhook', to: 'chatbot#webhook'
   
-  #scope :path => '/api/v1/', :module => "api_v1", :as => 'v1', :defaults => { :format => :json } do
+  scope : :defaults => { :format => :json } do
     get '/bargain', to: 'bargain#index'
     get '/bargain/end', to: 'bargain#game_end'
     get '/bargain/now', to: 'bargain#now_win_bid'
 
     post '/bargin', to: 'bargain#new_bid'
-  #end
+  end
 end
 
