d2660c8e7f7ab7896fc25645e71fe3f5382d76d5
Wols <caniculawolf@hotmail.com>
2019-03-11 21:44:26 +0800

    Update bargain_controller.rb

--- app/controllers/bargain_controller.rb
+++ app/controllers/bargain_controller.rb
@@ -1,9 +1,16 @@
-class BargainController < ActionController::API
+class Api::BargainController < ApplicationController
 # disable the CSRF token
 protect_from_forgery with: :null_session
 
 # disable cookies (no set-cookies header in response)
 before_action :destroy_session
+
+# disable the CSRF token
+skip_before_action :verify_authenticity_token
+
+    def destroy_session
+        request.session_options[:skip] = true
+    end
 
     def index
         #Bargain.start(channel_id)
