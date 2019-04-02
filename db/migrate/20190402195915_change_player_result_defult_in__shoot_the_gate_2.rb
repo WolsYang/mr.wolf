1a05e86b9e49dd881e03030f3ba29dbdfd13b1d2
Wols <caniculawolf@hotmail.com>
2019-04-03 04:00:05 +0800

    增加結果欄位預設值

---
+++ db/migrate/20190402195915_change_player_result_defult_in__shoot_the_gate_2.rb
@@ -0,0 +1,5 @@
+class ChangePlayerResultDefultInShootTheGate2 < ActiveRecord::Migration[5.2]
+  def change
+    change_column_default :shoot_the_gates, :player_result, [[0]]
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_02_133905) do
+ActiveRecord::Schema.define(version: 2019_04_02_195915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,7 +82,7 @@ ActiveRecord::Schema.define(version: 2019_04_02_133905) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "cards", default: [], array: true
-    t.text "player_result", array: true
+    t.text "player_result", default: [["0"]], array: true
   end
 
   create_table "users", force: :cascade do |t|
