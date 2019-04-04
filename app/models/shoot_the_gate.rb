6d55c04e799fad10f2ed060051b19afcdb533eae
Wols <caniculawolf@hotmail.com>
2019-04-04 17:22:05 +0800

    Update shoot_the_gate.rb

--- app/models/shoot_the_gate.rb
+++ app/models/shoot_the_gate.rb
@@ -207,7 +207,7 @@ class ShootTheGate < ApplicationRecord
     else 
       (1...game.player_result.size).each do |n| #game.player_result第一個直是預設的不用印
         n = game.player_result[n]
-        bet_rate = (game.player_result[0][1].to_i)game.player_result[0][2].to_i)* n[2].to_i
+        bet_rate = (game.player_result[0][1].to_i/game.player_result[0][2].to_i)* n[2].to_i
         message +=  "[玩家] : " + n[0] + " [籌碼數] : " + n[1] + " [參與局數] : " + n[2] + "[需貢獻獎金池] : " + bet_rate.to_s + "\n" 
       end
     end
